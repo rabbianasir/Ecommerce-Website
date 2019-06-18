ffbb78c71c91ae3162a999d21f3c01683ea3a847
RabbiaNasir <38103048+rabbianasir@users.noreply.github.com>
2019-06-18 21:27:12 +0500

    Update content.php

--- content.php
+++ content.php
@@ -1,274 +1,78 @@
 <?php
 /**
- * Display all excellent functions and definitions
+ * The template for displaying content.
  *
  * @package Theme Freesia
  * @subpackage Excellent
  * @since Excellent 1.0
  */
-
-/************************************************************************************************/
-if (!function_exists('excellent_setup')):
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function excellent_setup() {
-	/**
-	 * Set the content width based on the theme's design and stylesheet.
-	 */
-	global $content_width;
-	if (!isset($content_width)) {
-		$content_width = 790;
-	}
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'post-thumbnails' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
-
-	register_nav_menus( array(
-		'topmenu' => __( 'Top Menu', 'excellent' ),
-		'primary' => __( 'Main Menu', 'excellent' ),
-		'footermenu' => __( 'Footer Menu', 'excellent' ),
-		'social-link'  => __( 'Add Social Icons Only', 'excellent' ),
-	) );
-
-	/* 
-	* Enable support for custom logo. 
-	*
-	*/ 
-	add_theme_support( 'custom-logo', array(
-		'flex-width' => true, 
-		'flex-height' => true,
-	) );
-
-	//Indicate widget sidebars can use selective refresh in the Customizer. 
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/*
-	 * Switch default core markup for comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
-
-	/**
-	 * Add support for the Aside Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio', 'chat' ) );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'excellent_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-
-	add_editor_style( array( 'css/editor-style.css') );
-
-	/**
-	* Making the theme Woocommrece compatible
-	*/
-
-	add_theme_support( 'woocommerce' );
-	add_theme_support( 'wc-product-gallery-zoom' );
-	add_theme_support( 'wc-product-gallery-lightbox' );
-	add_theme_support( 'wc-product-gallery-slider' );
-}
-endif; // excellent_setup
-add_action( 'after_setup_theme', 'excellent_setup' );
-
-/***************************************************************************************/
-function excellent_content_width() {
-	if ( is_page_template( 'page-templates/gallery-template.php' ) || is_attachment() ) {
-		global $content_width;
-		$content_width = 1170;
-	}
-}
-add_action( 'template_redirect', 'excellent_content_width' );
-
-/***************************************************************************************/
-if (!function_exists('excellent_get_theme_options')):
-	function excellent_get_theme_options() {
-	    return wp_parse_args(  get_option( 'excellent_theme_options', array() ), excellent_get_option_defaults_values() );
-	}
-endif;
-
-/***************************************************************************************/
-require get_template_directory() . '/inc/customizer/excellent-default-values.php';
-require get_template_directory() . '/inc/settings/excellent-functions.php';
-require get_template_directory() . '/inc/settings/excellent-common-functions.php';
-require get_template_directory() . '/inc/jetpack.php';
-
-/************************ Excellent Sidebar  *****************************/
-require get_template_directory() . '/inc/widgets/widgets-functions/register-widgets.php';
-
-/************************ Excellent Customizer  *****************************/
-require get_template_directory() . '/inc/customizer/functions/sanitize-functions.php';
-require get_template_directory() . '/inc/customizer/functions/register-panel.php';
-
-function excellent_customize_register( $wp_customize ) {
-if (!class_exists('Excellent_Plus_Features')) {
-	class Excellent_Customize_upgrade extends WP_Customize_Control {
-		public function render_content() { ?>
-			<a title="<?php esc_html_e( 'Review Us', 'excellent' ); ?>" href="<?php echo esc_url( 'https://wordpress.org/support/view/theme-reviews/excellent/' ); ?>" target="_blank" id="about_excellent">
-			<?php esc_html_e( 'Review Us', 'excellent' ); ?>
-			</a><br/>
-			<a href="<?php echo esc_url( 'https://themefreesia.com/theme-instruction/excellent/' ); ?>" title="<?php esc_html_e( 'Theme Instructions', 'excellent' ); ?>" target="_blank" id="about_excellent">
-			<?php esc_html_e( 'Theme Instructions', 'excellent' ); ?>
-			</a><br/>
-			<a href="<?php echo esc_url( 'https://tickets.themefreesia.com/' ); ?>" title="<?php esc_html_e( 'Support Tickets', 'excellent' ); ?>" target="_blank" id="about_excellent">
-			<?php esc_html_e( 'Forum', 'excellent' ); ?>
-			</a><br/>
-			<a href="<?php echo esc_url( 'https://www.facebook.com/themefreesia/' ); ?>" title="<?php esc_html_e( 'Facebook', 'excellent' ); ?>" target="_blank" id="about_excellent">
-			<?php esc_html_e( 'Facebook', 'excellent' ); ?>
-			</a><br/>
-			<a href="<?php echo esc_url( 'https://twitter.com/themefreesia' ); ?>" title="<?php esc_html_e( 'Twitter', 'excellent' ); ?>" target="_blank" id="about_excellent">
-			<?php esc_html_e( 'Twitter', 'excellent' ); ?>
-			</a><br/>
-		<?php
-		}
-	}
-	$wp_customize->add_section('excellent_upgrade_links', array(
-		'title'					=> __('Important Links', 'excellent'),
-		'priority'				=> 1000,
-	));
-	$wp_customize->add_setting( 'excellent_upgrade_links', array(
-		'default'				=> false,
-		'capability'			=> 'edit_theme_options',
-		'sanitize_callback'		=> 'wp_filter_nohtml_kses',
-	));
-	$wp_customize->add_control(
-		new Excellent_Customize_upgrade(
-		$wp_customize,
-		'excellent_upgrade_links',
-			array(
-				'section'		=> 'excellent_upgrade_links',
-				'settings'		=> 'excellent_upgrade_links',
-			)
-		)
-	);
-}	
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-		
-	if (isset($wp_customize->selective_refresh)) {
-		$wp_customize->selective_refresh->add_partial( 'blogname', array(
-			'selector' => '.site-title a',
-			'container_inclusive' => false,
-			'render_callback' => 'excellent_customize_partial_blogname',
-		) );
-		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
-			'selector' => '.site-description',
-			'container_inclusive' => false,
-			'render_callback' => 'excellent_customize_partial_blogdescription',
-		) );
-	}
-	require get_template_directory() . '/inc/customizer/functions/frontpage-features.php' ;
-	require get_template_directory() . '/inc/customizer/functions/design-options.php';
-	require get_template_directory() . '/inc/customizer/functions/theme-options.php';
-	require get_template_directory() . '/inc/customizer/functions/featured-content-customizer.php' ;
-	require get_template_directory() . '/inc/customizer/functions/color-options.php' ;
-}
-
-if(!class_exists('Excellent_Plus_Features')){
-	// Add Upgrade to Plus Button.
-	require_once( trailingslashit( get_template_directory() ) . 'inc/upgrade-plus/class-customize.php' );
-}
-
-/* Color Styles */
-require get_template_directory() . '/inc/settings/color-option-functions.php';
-
-/** 
-* Render the site title for the selective refresh partial. 
-* @see excellent_customize_register() 
-* @return void 
-*/
-function excellent_customize_partial_blogname() { 
-	bloginfo( 'name' ); 
-} 
-
-/** 
-* Render the site tagline for the selective refresh partial. 
-* @see excellent_customize_register() 
-* @return void 
-*/ 
-function excellent_customize_partial_blogdescription() { 
-	bloginfo( 'description' ); 
-}
-add_action( 'customize_register', 'excellent_customize_register' );
-
-/******************* Excellent Header Display *************************/
-function excellent_header_display() {
-	$excellent_settings = excellent_get_theme_options();
-	$header_display = $excellent_settings['excellent_header_display'];
-	if ($header_display == 'header_logo' || $header_display == 'header_text' || $header_display == 'show_both')	{
-		echo '<div id="site-branding">';
-			if($header_display != 'header_text'){
-				excellent_the_custom_logo();
-			}
-
-			if (is_home() || is_front_page()){ ?>
-			<h1 id="site-title"> <?php }else{?> <h2 id="site-title"> <?php } ?>
-			<a href="<?php echo esc_url(home_url('/'));?>" title="<?php echo esc_html(get_bloginfo('name', 'display'));?>" rel="home"> <?php bloginfo('name');?> </a>
-			<?php if(is_home() || is_front_page()){ ?>
-			</h1>  <!-- end .site-title -->
-			<?php } else { ?> </h2> <!-- end .site-title --> <?php }
-
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ($site_description){?>
-			<div id="site-description"> <?php bloginfo('description');?> </div> <!-- end #site-description -->
-		<?php }
-		echo '</div>'; // end #site-branding
-	}
-}
-add_action('excellent_site_branding','excellent_header_display');
-
-if (!function_exists('excellent_the_custom_logo')): 
- 	/** 
- 	 * Displays the optional custom logo. 
- 	 * Does nothing if the custom logo is not available. 
- 	 */ 
- 	function excellent_the_custom_logo() { 
- 	    if (function_exists('the_custom_logo')) { 
- 	        the_custom_logo(); 
- 	    }
- 	}
-endif;
-
-/* Excellent Template */
-require get_template_directory() . '/inc/front-page/about-us.php';
-require get_template_directory() . '/inc/front-page/front-page-features.php';
-require get_template_directory() . '/inc/front-page/latest-from-blog.php';
-require get_template_directory() . '/inc/front-page/portfolio-box.php';
-require get_template_directory() . '/inc/front-page/our-testimonial.php';
-
-/************** Footer Menu *************************************/
-function excellent_footer_menu_section() {
-	if (has_nav_menu('footermenu')):
-		$args = array(
-			'theme_location' => 'footermenu',
-			'container'      => '',
-			'items_wrap'     => '<ul>%3$s</ul>',
-		);
-		echo '<nav id="footer-navigation">';
-		wp_nav_menu($args);
-		echo '</nav><!-- end #footer-navigation -->';
-	endif;
-}
-add_action( 'excellent_footer_menu', 'excellent_footer_menu_section' );
+$excellent_settings = excellent_get_theme_options(); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class();?>>
+		<?php $excellent_blog_post_image = $excellent_settings['excellent_blog_post_image'];
+		if( has_post_thumbnail() && $excellent_blog_post_image == 'on') { ?>
+			<div class="post-image-content">
+				<figure class="post-featured-image">
+					<a href="<?php the_permalink();?>" title="<?php echo the_title_attribute('echo=0'); ?>">
+					<?php the_post_thumbnail(); ?>
+					</a>
+				</figure><!-- end.post-featured-image  -->
+			</div> <!-- end.post-image-content -->
+		<?php } ?>
+		<header class="entry-header">
+		<?php  $entry_format_meta_blog = $excellent_settings['excellent_entry_meta_blog'];
+			if($entry_format_meta_blog == 'show-meta' ){?>
+			<div class="entry-meta">
+				<?php $format = get_post_format();
+				if ( current_theme_supports( 'post-formats', $format ) ) {
+					printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+						sprintf( ''),
+						esc_url( get_post_format_link( $format ) ),
+						get_post_format_string( $format )
+					);
+				} ?>
+				<span class="cat-links">
+					<?php the_category(', '); ?>
+				</span>
+				<!-- end .cat-links -->
+				<?php $tag_list = get_the_tag_list( '', __( ', ', 'excellent' ) );
+				if(!empty($tag_list)){ ?>
+					<span class="tag-links">
+					<?php   echo get_the_tag_list( '', __( ', ', 'excellent' ) ); ?>
+					</span> <!-- end .tag-links -->
+				<?php }  ?>
+			</div>
+			<?php } ?>
+			<h2 class="entry-title"> <a href="<?php the_permalink(); ?>" title="<?php echo the_title_attribute('echo=0'); ?>"> <?php the_title();?> </a> </h2> <!-- end.entry-title -->
+			<?php if($entry_format_meta_blog == 'show-meta' ){ ?> 
+				<div class="entry-meta">
+					<span class="author vcard"><span><?php esc_html_e('by','excellent');?></span><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php the_author(); ?>">
+					<?php the_author(); ?> </a></span>
+					<span class="posted-on"><a title="<?php echo esc_attr( get_the_time() ); ?>" href="<?php the_permalink(); ?>">
+					<?php the_time( get_option( 'date_format' ) ); ?> </a></span>
+					<?php if ( comments_open() ) { ?>
+					<span class="comments"><i class="fa fa-comment-o"></i>
+					<?php comments_popup_link( __( 'No Comments', 'excellent' ), __( '1 Comment', 'excellent' ), __( '% Comments', 'excellent' ), '', __( 'Comments Off', 'excellent' ) ); ?> </span>
+					<?php } ?>
+				</div><!-- end .entry-meta -->
+			<?php } ?>
+			</header><!-- end .entry-header -->
+			<div class="entry-content">
+			<?php
+			$excellent_tag_text = $excellent_settings['excellent_tag_text'];
+			$content_display = $excellent_settings['excellent_blog_content_layout'];
+			if($content_display == 'excerptblog_display'):
+					the_excerpt(); ?>
+				<a href="<?php echo esc_url(get_permalink());?>" class="more-link"><?php echo esc_attr($excellent_tag_text);?></a>
+				<?php else:
+					the_content( sprintf($excellent_tag_text));
+				endif; ?>
+			</div> <!-- end .entry-content -->
+			<?php wp_link_pages( array( 
+					'before'            => '<div style="clear: both;"></div><div class="pagination clearfix">'.esc_html__( 'Pages:', 'excellent' ),
+					'after'             => '</div>',
+					'link_before'       => '<span>',
+					'link_after'        => '</span>',
+					'pagelink'          => '%',
+					'echo'              => 1
+				) ); ?>
+		</article><!-- end .post -->
